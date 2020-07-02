dd9aeb0682388cc62da4227663345d0f32b726d7
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-02 12:18:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
     strip.show()
 })
 input.onButtonPressed(Button.B, function () {
